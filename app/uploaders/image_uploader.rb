9294156fdeac55b6ef9425d845fbe6e468e15144
Valentine <vshlyagin@inbox.ru>
2023-01-06 22:43:05 +0300

    update vaidates image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,7 +14,7 @@ class ImageUploader < Shrine
         end
 
     Attacher.validate do
-       validate_max_size 1.megabyte, message: "is too large (max is 1 MB)"
+       validate_max_size 5.megabyte, message: "is too large (max is 5 MB)"
        validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png']
     end
 end
